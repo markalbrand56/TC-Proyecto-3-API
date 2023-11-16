5d691cd25d0256650b0dbef1b8e0a62d0679f802
Mark Albrand <mark.albrand5@gmail.com>
2023-11-16 17:02:15 -0600

    API

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/turingMachine/{cadena}")
+async def turning_machine(cadena: str):
+    # Define the Turing Machine configuration based on the provided JSON
+    with open("turing.json") as config_file:
+        config = json.load(config_file)
+
+    # Create an instance of the Turing Machine
+    turing_machine = TuringMachine(**config)
+
+    simulationResult, isAccepted = turing_machine.simulate(cadena)
+
+    return {"Resultado: ": simulationResult, "esAceptado": isAccepted}
